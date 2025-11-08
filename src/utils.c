439db52922486eea3ea2983783f011e65d9b30d9
Andrea Blushi <andreablushi@gmail.com>
2025-11-08 10:51:02 +0100

    Created reset_accumulator function

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,8 @@
 #include <headers/utils.h>
 
+/*
+    Helper function to free a pointer and set it to NULL
+*/
 static inline void free_and_null(void **p) {
     if (p && *p) {
         free(*p);
@@ -33,4 +36,13 @@ void safe_cleanup(
     free_and_null((void**)N_k);
     free_and_null((void**)mu_k);
     free_and_null((void**)sigma_k);
+}
+
+/*
+    Reset accumulators used in the M-step of the EM algorithm
+*/
+void reset_accumulators(double *N_k, double *mu_k, double *sigma_k, int K, int D) {
+    memset(N_k, 0, (size_t)K * sizeof(double));
+    memset(mu_k, 0, (size_t)K * D * sizeof(double));
+    memset(sigma_k, 0, (size_t)K * D * sizeof(double));
 }
