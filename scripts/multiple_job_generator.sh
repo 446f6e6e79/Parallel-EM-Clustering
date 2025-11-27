3f32ef40cd5a2cdb725872170ba73b77f3e84b8f
446f6e6e79 <davidedona03@gmail.com>
2025-11-27 19:23:15 +0100

    Improved job launchers

--- scripts/multiple_job_generator.sh
+++ scripts/multiple_job_generator.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
+# === Parameters ===
+ITERATION_PER_COMBO=3
+DATASET_INITIAL_NAME="d" # As example, if your datasets are d_1, d_2, d_3 set it to d
+MEM="64gb" # Memory per NODE
+PLACEMENT="pack:excl"
+
+# NODES:NCPUS:THREADS
+COMBOS=( 
+  "1:1:1"
+  "1:1:2"
+  "1:1:4"
+  "1:1:8"
+  "2:1:8"
+  "2:2:8"
+  "4:2:8"
+)
+
+SHORT_QUEUE="short_HPC4DS"
+LONG_QUEUE="long_cpuQ"
+SHORT_WALLTIME="06:00:00"
+LONG_WALLTIME="10:00:00"
 
 # === Common parameters ===
 BASE_DIR="$HOME/Parallel-EM-Clustering"
-MEM="64gb"
-PLACEMENT="pack:excl"
 EXECUTABLE="${BASE_DIR}/bin/EM_Clustering"
 TEMPLATE="${BASE_DIR}/scripts/job_template.sh"
+
 DATASETS_DIR="${BASE_DIR}/data/datasets"
-
-OUTPUT_DIR="${BASE_DIR}/jobs"
-mkdir -p "$OUTPUT_DIR"
-
 # Detect datasets
-DATASETS=($(find "$DATASETS_DIR" -maxdepth 1 -type d -name "d_*" | sort))
+DATASETS=($(find "$DATASETS_DIR" -maxdepth 1 -type d -name "${DATASET_INITIAL_NAME}*" | sort))
 if [ ${#DATASETS[@]} -eq 0 ]; then
   echo "No dataset directories found in $DATASETS_DIR"
   exit 1
@@ -25,18 +41,12 @@ if [ ! -f "$OUTPUT_INFO" ]; then
   exit
 fi
 
-# === Define the combinations ===
-COMBOS=(
-  "1:1:1"
-  "1:1:2"
-  "1:1:4"
-  "1:1:8"
-  "2:1:8"
-  "2:2:8"
-  "4:2:8"
-)
+# Create all the output directories for the jobs
+OUTPUT_DIR="${BASE_DIR}/jobs"
+mkdir -p "$OUTPUT_DIR/long" "$OUTPUT_DIR/short"
 
-for run in {1..3}; do
+# === Define the combinations ===
+for run in $(seq 1 "$ITERATION_PER_COMBO"); do
   echo "=== Generating jobs for iteration $run ==="
 
   for DATA_DIR in "${DATASETS[@]}"; do
@@ -55,20 +65,23 @@ for run in {1..3}; do
     for combo in "${COMBOS[@]}"; do
 
       IFS=":" read -r NODES NCPUS THREADS <<< "$combo"
+
       TOTAL_PROCESSES=$(( NCPUS * THREADS ))
       NP=$((NCPUS * NODES))
       
       # Choose the queue and the walltime based on the n_process
-      if [ "$TOTAL_PROCESSES" -le 2 ]; then
-          QUEUE="long_cpuQ"
-          WALLTIME="10:00:00"
+      if [ "$TOTAL_PROCESSES" -le 2 ] && [[ "$dataset_name" == *_1 ]]; then
+          QUEUE="$LONG_QUEUE"
+          WALLTIME="$LONG_WALLTIME"
+          CURRENT_OUTPUT_DIR="${OUTPUT_DIR}/long"
       else
-          QUEUE="short_HPC4DS"
-          WALLTIME="06:00:00"
+          QUEUE="$SHORT_QUEUE"
+          WALLTIME="$SHORT_WALLTIME"
+          CURRENT_OUTPUT_DIR="${OUTPUT_DIR}/short"
       fi
       PARAMETERS="-i $input -m $meta -b $OUTPUT_INFO -n $THREADS"
 
-      JOB_SCRIPT="${OUTPUT_DIR}/job_${dataset_name}-run_${run}-nodes_${NODES}-cpus_${NCPUS}-threads_${THREADS}.sh"
+      JOB_SCRIPT="${CURRENT_OUTPUT_DIR}/job_${dataset_name}-run_${run}-nodes_${NODES}-cpus_${NCPUS}-threads_${THREADS}.sh"
 
       sed "s|__EXECUTABLE__|$EXECUTABLE|g; \
            s|__PLACEMENT__|$PLACEMENT|g; \
